6c522ef9e1a8261b75867bb385dc8d555f744635
David Montesdeoca <backpackerhh@gmail.com>
2024-03-01 17:09:37 +0100

    Add validator to ensure data received in a request has expected structure
    This validator is used for defining the schemas in Swagger documentation
    as well.
    
    The schemas are stored as JSON files in contexts/shared/infrastructure/schemas.
    
    References:
    
    * https://github.com/voxpupuli/json-schema

---
+++ contexts/shared/infrastructure/schema_validator.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "json-schema"
+
+module Shared
+  module Infrastructure
+    module SchemaValidator
+      def self.validate(schema_name, data)
+        JSON::Validator.fully_validate(load_schema(schema_name), data, errors_as_objects: true)
+      end
+
+      def self.load_schema(schema_name)
+        schema_file = File.expand_path("schemas/#{schema_name}_schema.json", __dir__)
+        content = File.read(schema_file)
+
+        JSON.parse(content)
+      end
+    end
+  end
+end
--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -34,52 +34,8 @@ RSpec.configure do |config|
           }
         },
         schemas: {
-          new_safebox: {
-            type: :object,
-            properties: {
-              data: {
-                type: :object,
-                properties: {
-                  id: { type: :string, format: :uuid, example: "f626c808-648c-41fe-865d-c6062f3e0899" },
-                  type: { type: :string, example: "safebox" },
-                  attributes: {
-                    type: :object,
-                    properties: {
-                      name: { type: :string, example: "Secure safebox 01" },
-                      password: { type: :string, example: "extremelySecurePassword" }
-                    },
-                    required: %w[name password]
-                  }
-                },
-                required: %w[id type attributes]
-              }
-            },
-            required: %w[data]
-          },
-          api_error: {
-            type: :object,
-            properties: {
-              errors: {
-                type: :array,
-                items: {
-                  properties: {
-                    id: { type: :string, format: :uuid, example: "9a80bb9b-3e9c-4eef-83b6-12990cf0aab2" },
-                    title: { type: :string, example: "Unknown attribute" },
-                    detail: { type: :string, example: "Unknown attribute: 'userId'" },
-                    status: { type: :string, example: "400" },
-                    code: { type: :string, example: "101" },
-                    source: {
-                      type: :object,
-                      properties: {
-                        pointer: { type: :string, example: "data/attributes/userId" }
-                      }
-                    }
-                  },
-                  required: [:title]
-                }
-              }
-            }
-          }
+          new_safebox: Shared::Infrastructure::SchemaValidator.load_schema("new_safebox"),
+          api_error: Shared::Infrastructure::SchemaValidator.load_schema("api_error")
         }
       },
       servers: [
